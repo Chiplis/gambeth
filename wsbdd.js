9ab704804734803fc5720d69b33453b9226c1ec9
Nicolás Siplis <5803723+nicolas-siplis@users.noreply.github.com>
2021-10-29 01:39:40 +0200

    Fix messages.

--- wsbdd.js
+++ wsbdd.js
@@ -450,9 +450,9 @@ function changeBetType() {
 
     createBetWolfram.style.display = betType != "WA" ? "none" : "block";
 
-    [createBetUrl, createBetPath, createBetQuery]
-        .forEach(elm => [elm.style.display, document.querySelector(`label[for="${elm.id}"]`).style.display] = Array(2).fill("WA" ? "none" : "block"));
-
+    [createBetUrl, createBetPath]
+        .forEach(elm => [elm.style.display, document.querySelector(`label[for="${elm.id}"]`).style.display] = Array(2).fill(betType == "WA" ? "none" : "block"));
+    createBetQuery.style.display = betType == "WA" ? "none" : "block";
     createBetQueryInner.innerHTML = parseBetQuery(createBetSchema.value, createBetUrl.value, createBetPath.value);
     switch (betType) {
         case "XML":
