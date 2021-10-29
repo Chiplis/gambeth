a2b7bd2a62cc67e8adec2827988793dad1cbcb41
Nicolás Siplis <5803723+nicolas-siplis@users.noreply.github.com>
2021-10-29 02:51:21 +0200

    Fix max price discovery

--- wsbdd.js
+++ wsbdd.js
@@ -87,8 +87,7 @@ const createBetBtn = async () => {
     [betContainer.style.display, newBet.style.display] = ['none', 'flex'];
     steps[currentStep].style.position = "initial";
     steps[currentStep].style.opacity = "100%";
-    console.log(await contract.lastQueryPrice("URL"));
-    const prices = (await Promise.all(["URL", "WolframAlpha"].map(contract.lastQueryPrice))).map(weiToEth).map(price => price.toString());
+    const prices = (await Promise.all(["URL", "WolframAlpha"].map(t => contract.lastQueryPrice(t)))).map(weiToEth).map(price => price.toString());
     (async () => {
         createBetAmountLabel.innerHTML = `The <a href="https://provable.xyz">oracle service</a> that WSBDD uses to interact with the web needs to be paid for by the bet's creator.` + (contract
             ? ` The suggested amount is ${Math.max(prices[0], prices[1])} ETH.`
