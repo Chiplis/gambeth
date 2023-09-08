cee6f61a84e8f78eafdcee2249ec8f2f4df0fd4c
N. Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2023-09-08 12:40:10 +0200

    Payoff -> Payout.

--- gambeth.js
+++ gambeth.js
@@ -837,14 +837,13 @@ async function renderBetPool() {
         marketPrices.innerHTML = ``;
         if (contractPrices) {
             const betChoices = await activeBetChoices();
-            const payoff = async outcome => (Number(await activeContract.calculateCost(activeBet)) / Number(await activeContract.resultPools(activeBet, outcome)));
-            console.log("Payoff", await payoff("a"));
+            const payout = async outcome => (Number(await activeContract.calculateCost(activeBet)) / Number(await activeContract.resultPools(activeBet, outcome)));
             const calcPrices = await Promise.all(betChoices.map(calculatePrice));
             const prices = calcPrices.map(async (p, i) => {
                 const outcome = betChoices[i];
                 const mktPrice = Math.round(p * 100) / 100;
                 const odds = (Math.pow(p, 2) * 100).toFixed(2);
-                const pay = (await payoff(betChoices[i])).toFixed(2);
+                const pay = (await payout(betChoices[i])).toFixed(2);
                 const owned = await activeContract.userBets(activeBet, owner, outcome).then(tokenToNumber).then(Math.round);
                 const avgPrice = await activeContract.userTransfers(activeBet, owner, outcome).then(tokenToNumber).then(a => Math.round(Number(a) / owned * 100) / 100);
                 return `<tr><td>${outcome}</td><td>${owned || "-"}</td><td>${odds}%</td><td>$${mktPrice}</td><td>${Number.isNaN(avgPrice) ? "-" : ("$" + avgPrice)}</td><td>$${pay}</td></tr>`
