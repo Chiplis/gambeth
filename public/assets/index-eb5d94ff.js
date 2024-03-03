dd0c7fa57bc240339cea911cc520015172c2de30
N. Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2024-03-03 19:44:07 -0300

    Update frontend.

---
+++ public/assets/index-eb5d94ff.js
@@ -0,0 +1,9 @@
+import{b9 as t,aH as e,bd as n,be as i,bg as s,ba as f,bf as d,bc as c,bb as l}from"./index-26f985ce.js";const a=`
+@font-face {
+  font-family: 'Inter';
+  font-style:  normal;
+  font-weight: 300 600;
+  font-display: swap;
+  src: url("https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19") format("woff2");
+}
+`;export{a as InterVar,t as ProviderRpcError,e as ProviderRpcErrorCode,n as chainIdValidation,i as chainNamespaceValidation,s as chainValidation,f as createEIP1193Provider,d as providerConnectionInfoValidation,c as validate,l as weiToEth};
