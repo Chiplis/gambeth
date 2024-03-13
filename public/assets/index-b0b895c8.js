99681f2690dcaf471e66f90485aa93eb6c8b47c8
N. Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2024-03-13 16:54:14 -0300

    Update frontend.

--- public/assets/index-b0b895c8.js
+++ public/assets/index-b0b895c8.js
@@ -1,4 +1,4 @@
-import{n as _t,s as ce,T as N,t as U,a as C,o as ot,R as Bo,p as q,y as gt}from"./index-626398da.js";import{d as Do}from"./dijkstra-f906a09e.js";import"./index-f4f5d3c5.js";/**
+import{n as _t,s as ce,T as N,t as U,a as C,o as ot,R as Bo,p as q,y as gt}from"./index-83c8dae8.js";import{d as Do}from"./dijkstra-f906a09e.js";import"./index-823e4818.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
