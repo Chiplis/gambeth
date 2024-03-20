1d5e68316f4c0d71330905235f869982dffe0687
N. Siplis <25803723+nicolas-siplis@users.noreply.github.com>
2024-03-20 08:48:55 -0300

    Update frontend.

--- public/assets/index-f77c6b86.js
+++ public/assets/index-f77c6b86.js
@@ -1,4 +1,4 @@
-import{P as t,n,p as e,q as i,x as s,m as f,u as d,o as c,w as l}from"./index-d2b154c5.js";const a=`
+import{P as t,n,p as e,q as i,x as s,m as f,u as d,o as c,w as l}from"./index-f932802d.js";const a=`
 @font-face {
   font-family: 'Inter';
   font-style:  normal;
